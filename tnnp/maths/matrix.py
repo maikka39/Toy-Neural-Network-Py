53e50a4e3a65993a73c543c35cca4438dd6dd21b
Maik de Kruif <maikka39@gmail.com>
2019-04-13 23:11:23 +0200

    Added matrix product

--- tnnp/maths/matrix.py
+++ tnnp/maths/matrix.py
@@ -30,3 +30,21 @@ class Matrix(object):
         """Randomize all values in the matrix."""
         self.matrix = [[randint(smallest, biggest) for value in self.matrix[row]]
                        for row in range(len(self.matrix))]
+
+
+def multiply(a, b):
+    """Matrix product of two matrices."""
+    if a.cols != b.rows:
+        raise Exception(
+            "Amount of rows of b must match amount of columns of a")
+
+    result = Matrix(a.rows, b.cols)
+
+    for i in range(result.rows):
+        for j in range(result.cols):
+            sum = 0
+            for k in range(a.cols):
+                sum += a.matrix[i][k] * b.matrix[k][j]
+            result.matrix[i][j] = sum
+
+    return result
