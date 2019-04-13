4f600f77a825d07d3e5191f5c17a38bf48208b01
Maik de Kruif <maikka39@gmail.com>
2019-04-13 21:45:49 +0200

    Added a tiny bit of documentation

--- tnnp/maths/matrix.py
+++ tnnp/maths/matrix.py
@@ -12,10 +12,12 @@ class Matrix(object):
                        for row in range(self.rows)]
 
     def multiply(self, n):
+        """Multiply all values in the matrix by a number."""
         self.matrix = [[value * n for value in self.matrix[row]]
                        for row in range(len(self.matrix))]
 
     def add(self, n):
+        """Add a number or matrix to all values in the matrix."""
         if isinstance(n, Matrix):
             self.matrix = [[value + n.matrix[row][column] for column, value in enumerate(self.matrix[row])]
                            for row in range(len(self.matrix))]
@@ -25,5 +27,6 @@ class Matrix(object):
                        for row in range(len(self.matrix))]
 
     def randomize(self, smallest=1, biggest=10):
+        """Randomize all values in the matrix."""
         self.matrix = [[randint(smallest, biggest) for value in self.matrix[row]]
                        for row in range(len(self.matrix))]
