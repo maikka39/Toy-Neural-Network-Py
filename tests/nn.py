9ae73d7d3787799782b86998c07eff7a74fd1ead
Maik de Kruif <maikka39@gmail.com>
2019-04-14 18:54:27 +0200

    Implemented the feedforward function

--- tests/nn.py
+++ tests/nn.py
@@ -1,3 +1,15 @@
-from tnnp import nn
+from tnnp import nn as tnnp
 
-neuralnetwork = nn.NeuralNetwork(3, 3, 1)
+nn = tnnp.NeuralNetwork(2, 2, 1)
+if nn is None:
+    raise Exception("Initialization failed!", m.matrix)
+
+nn = tnnp.NeuralNetwork(2, 2, 1)
+input = [1, 0]
+output = nn.feedforward(input)
+print(output)
+if output < [-1] or output > [1]:
+    raise Exception("feedforward failed!", m.matrix)
+
+
+print("No errors were found!")
--- tnnp/nn.py
+++ tnnp/nn.py
@@ -1,3 +1,10 @@
+from math import exp
+
+import tnnp.maths.matrix as matrix
+
+# from tnnp.maths.matrix import Matrix, multiply, transpose
+
+
 class NeuralNetwork(object):
     """Multi Layer NeuralNetwork."""
 
@@ -5,3 +12,35 @@ class NeuralNetwork(object):
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
+
+        self.weights_ih = matrix.Matrix(self.hidden_nodes, self.input_nodes)
+        self.weights_ho = matrix.Matrix(self.output_nodes, self.hidden_nodes)
+        self.weights_ih.randomize()
+        self.weights_ho.randomize()
+
+        self.bias_h = matrix.Matrix(self.hidden_nodes, 1)
+        self.bias_o = matrix.Matrix(self.output_nodes, 1)
+        self.bias_h.fill(1)
+        self.bias_o.fill(1)
+
+    def feedforward(self, input_arr):
+        """Returns the guessed output from the input"""
+        input = matrix.from_array(input_arr)
+
+        hidden = matrix.multiply(self.weights_ih, input)
+        hidden.add(self.bias_h)
+        hidden.map(sign)
+
+        output = matrix.multiply(self.weights_ho, hidden)
+        output.add(self.bias_o)
+        output.map(sign)
+
+        return output.to_array()
+
+    # Set aliasses
+    guess = feedforward
+
+
+def sign(n):
+    """The activation function"""
+    return 1 / (1 + exp(-n))
