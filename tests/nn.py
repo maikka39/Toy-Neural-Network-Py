5856836edf38772e7009405cafbafcce33537b69
Maik de Kruif <maikka39@gmail.com>
2019-04-14 23:10:03 +0200

    Added train function to test script

--- tests/nn.py
+++ tests/nn.py
@@ -1,3 +1,5 @@
+import random
+
 from tnnp import nn as tnnp
 
 nn = tnnp.NeuralNetwork(2, 2, 1)
@@ -7,9 +9,38 @@ if nn is None:
 nn = tnnp.NeuralNetwork(2, 2, 1)
 input = [1, 0]
 output = nn.feedforward(input)
-print(output)
 if output < [-1] or output > [1]:
-    raise Exception("feedforward failed!", m.matrix)
+    raise Exception("feedforward function failed!", m.matrix)
 
+
+training_data = [
+    {
+        "inputs": [0, 0],
+        "targets": [0]
+    },
+    {
+        "inputs": [0, 1],
+        "targets": [1]
+    },
+    {
+        "inputs": [1, 0],
+        "targets": [1]
+    },
+    {
+        "inputs": [1, 1],
+        "targets": [0]
+    }
+]
+nn = tnnp.NeuralNetwork(2, 2, 1)
+for i in range(100000):
+    data = random.choice(training_data)
+    nn.train(data.get("inputs"), data.get("targets"))
+values = []
+for data in training_data:
+    output = nn.feedforward(data.get("inputs"))
+    values.append(round(output[0]))
+if not values == [0, 1, 1, 0]:
+    raise Exception(
+        "train function failed! You might want to try running this script again.", values)
 
 print("No errors were found!")
